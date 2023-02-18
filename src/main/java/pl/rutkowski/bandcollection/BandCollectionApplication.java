8becf4ad08575afda7a190f76c5b3079522da642
adrianrut <adrianrutkowskiarcgas@gmail.com>
2023-02-18 11:58:56 +0100

    Project create

---
+++ src/main/java/pl/rutkowski/bandcollection/BandCollectionApplication.java
@@ -0,0 +1,13 @@
+package pl.rutkowski.bandcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BandCollectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/rutkowski/bandcollection/BandCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rutkowski.bandcollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandCollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
